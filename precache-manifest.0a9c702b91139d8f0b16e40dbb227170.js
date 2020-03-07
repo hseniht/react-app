4d837788858619c35f094048784ad5ab7a175e4a
hseniht <thinesh_kumar_mu@hotmail.com>
2020-03-08 01:57:06 +0800

    Updates

---
+++ precache-manifest.0a9c702b91139d8f0b16e40dbb227170.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c8a27b9124436c3365cd528feb31a41",
+    "url": "/react-app/index.html"
+  },
+  {
+    "revision": "b3aa9f80da245ca6cfe3",
+    "url": "/react-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "75706be64a26e478aa24",
+    "url": "/react-app/static/js/2.ec4e1ab1.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/react-app/static/js/2.ec4e1ab1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b3aa9f80da245ca6cfe3",
+    "url": "/react-app/static/js/main.94b4a5eb.chunk.js"
+  },
+  {
+    "revision": "2e596b3b5041debf5e7a",
+    "url": "/react-app/static/js/runtime-main.2f7d3d91.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
